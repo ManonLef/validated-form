13f0f8c966c6e0b482e0aa8ca2bdd125a6bf0b7c
Manon <manonlefkos@gmail.com>
2023-04-23 23:59:54 +0200

    edit country guidance for zip

--- script.js
+++ script.js
@@ -44,27 +44,25 @@ function checkZIP() {
   const constraints = {
     ch: [
       "^(CH-)?\\d{4}$",
-      "Switzerland ZIPs must have exactly 4 digits: e.g. CH-1950 or 1950",
+      "CH: 4 digits: e.g. CH-1950 or 1950",
     ],
     fr: [
       "^(F-)?\\d{5}$",
-      "France ZIPs must have exactly 5 digits: e.g. F-75012 or 75012",
+      "FR: 5 digits: e.g. F-75012 or 75012",
     ],
     de: [
       "^(D-)?\\d{5}$",
-      "Germany ZIPs must have exactly 5 digits: e.g. D-12345 or 12345",
+      "DE: 5 digits: e.g. D-12345 or 12345",
     ],
     nl: [
       "^(NL-)?\\d{4}\\s*([A-RT-Z][A-Z]|S[BCE-RT-Z])$",
-      "NL ZIPs must have exactly 4 digits, followed by 2 letters except SA, SD and SS",
+      "NL: 4 digits, followed by 2 letters except SA, SD and SS",
     ],
   };
 
   const country = document.getElementById("country").value;
   const constraint = new RegExp(constraints[country][0], "")
   const constraintGuide = (constraints[country][1])
-
-  console.log(constraint)
 
   if (constraint.test(zip.value)) {
     zipError.textContent = `✓ ${constraintGuide}`
